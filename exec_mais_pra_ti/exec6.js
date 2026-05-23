3e0ae20aa8d62c221e0ccef8d3fce94cc9527444
Cezaniltom <cezaniltom@outlook.com>
2026-05-23 00:26:43 -0300

    realizando o exercicio 06 do mais pra ti

--- exec_mais_pra_ti/exec6.js
+++ exec_mais_pra_ti/exec6.js
@@ -12,3 +12,25 @@ const prompt = require('prompt-sync')()
 const trianguloA = Number(prompt('Informe o lado A '))
 const trianguloB = Number(prompt('Informe o lado B '))
 const trianguloC = Number(prompt('Informe o lado C '))
+
+if (
+    trianguloA < trianguloB + trianguloC &&
+    trianguloB < trianguloA + trianguloC &&
+    trianguloC < trianguloA + trianguloB
+    ) {
+    console.log('\nOs lados informados formam um triângulo!')
+
+    if (trianguloA === trianguloB && trianguloB === trianguloC) {
+        console.log('Tipo: Triângulo Equilátero')
+    } else if (
+        trianguloA === trianguloB ||
+        trianguloA === trianguloC ||
+        trianguloB === trianguloC
+    ) {
+        console.log('Tipo: Triângulo Isósceles')
+    } else {
+        console.log('Tipo: Triângulo Escaleno')
+    }
+    } else {
+    console.log('\nOs lados informados NÃO formam um triângulo.')
+}
