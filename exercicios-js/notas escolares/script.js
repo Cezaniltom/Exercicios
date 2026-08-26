60a8340b44be29f637fd0d4e4f4932624a537a21
Cezaniltom <cezaniltom@outlook.com>
2026-08-25 21:49:03 -0300

    Create descrition question school score

---
+++ exercicios-js/notas escolares/script.js
@@ -0,0 +1,7 @@
+// Criar um algoritmo que transforma as notas do sistema numérico para sistema de notas em caracteres tipo A B C D E F
+
+// 90 pra cima = A
+// entre 80 - 89 = B
+// entre 70 - 79 = C
+// entre 60 - 69 = D
+// menor que 60 = F
