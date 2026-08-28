a988c37165be4411f1a645af28446cd49709ee03
Cezaniltom <cezaniltom@outlook.com>
2026-08-28 10:38:54 -0300

    Create container component

--- exercicios-react/src/App.tsx
+++ exercicios-react/src/App.tsx
@@ -1,15 +1,27 @@
-import { Heading } from './components/heading/Heading'
+// import { Heading } from './components/heading/Heading'
+import { Container  } from './components/container/Container'
 import './styles/global.css'
 import './styles/theme.css'
 
 export function App() {
-  return <>
-    <Heading
-      header={<h2>Teste 1</h2>}
-    >
-      <header>{<h2>Teste 2</h2>}</header>
-      <header>{<h2>Teste 3</h2>}</header>
-      <header>{<h2>Aviso</h2>}</header>
-    </Heading>
-  </>
-}
+  return(
+    <>
+      {/* <Heading
+        header={<h2>Teste 1</h2>}
+      >
+        <header>{<h2>Teste 2</h2>}</header>
+        <header>{<h2>Teste 3</h2>}</header>
+        <header>{<h2>Aviso</h2>}</header>
+      </Heading> */}
+
+      <Container>
+        <section>LOGO</section>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
+      <Container>
+        <section>FORM</section>
+      </Container>
+    </>
+)}
---
+++ exercicios-react/src/components/container/Container.tsx
@@ -0,0 +1,16 @@
+import { type ReactNode } from "react";
+import styles from './Container.module.css'
+
+interface ContainerComponent {
+    children: ReactNode
+}
+
+export function Container({children}: ContainerComponent) {
+    return (
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    {children}
+                </div>
+            </div>
+    )
+}
