1262d604e3bc5807903a981aa3286757dedabb7d
Cezaniltom <cezaniltom@outlook.com>
2026-08-31 12:29:36 -0300

    Adding rest operator

--- exercicios-react/src/components/inputForm/InputForm.tsx
+++ exercicios-react/src/components/inputForm/InputForm.tsx
@@ -7,15 +7,17 @@ type InputFormProps = {
     error?: string
 } & React.ComponentProps<'input'> //Usado para obter todos os resultados de um elemento/atributo
 
-export function InputForm({name, label, type, error}: InputFormProps) {
+export function InputForm({name, label, type, error, ...rest}: InputFormProps) {
     return (
         <div>
-            {label ? <label htmlFor={name} className={styles.label}>{label}</label> : ''}
+            {/* Aqui funciona como um operador ternario, mas ao inves de usar ? e :, como o valor não tem falso, substitui por && */}
+            {label && <label htmlFor={name} className={styles.label}>{label}</label>}
             <input
                 id={name}
                 name={name}
                 type={type}
                 className={styles.inputField}
+                {...rest} // Significa que posso passar o restante dos atributos do input
             />
 
             {error && <span className={styles.span}>{error}</span>}
